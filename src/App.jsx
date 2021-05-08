32ff7ae9273b3908e9db6e7283b1f86640c831b6
mize <mize@bytedance.com>
2021-05-08 21:12:13 +0800

    store & listeners & subscribe: only render connected components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import React, { Component, useContext, useState } from 'react'
+import React, { Component, useContext, useEffect, useState } from 'react'
 
 const appContext = React.createContext(null)
+const store = {
+  state: {
+    user: { name: 'mize', age: 20 }
+  },
+  setState(newState) {
+    store.state = newState
+    store.listeners.map(fn => fn(store.state))
+  },
+  listeners: [],
+  subscribe(fn) {
+    const l = store.listeners;
+    console.log(l)
+    l.push(fn)
+    return () => {
+      const i = l.indexOf(fn)
+      l.splice(i, 1)
+    }
+  }
+}
+
+const connect = (Component) => (props) => {
+  const { state, setState } = useContext(appContext)
+  const [, update] = useState({})
+  useEffect(() => {
+    store.subscribe(() => {
+      update({})
+    })
+  }, [])
+  const dispatch = (action) => {
+    setState(reducer(state, action))
+  }
+  return <Component {...props} dispatch={dispatch} state={state} />
+}
 
 export const App = () => {
-  const [state, setState] = useState({
-    user: { name: 'mize', age: 20 }
-  })
-
-  const contextValue = { state, setState }
-
-  console.log(contextValue)
-
   return (
-    <appContext.Provider value={contextValue}>
+    <appContext.Provider value={store}>
       <A />
       <B />
       <C />
@@ -20,12 +45,23 @@ export const App = () => {
   )
 }
 
-const A = () => <section>A <User /></section>
-const B = () => <section>B <UserModifier name='Input' /> </section>
-const C = () => <section>C <User /></section>
-const User = () => {
-  const contextValue = useContext(appContext)
-  return <div>User: {contextValue.state.user.name}</div>
+const A = () => {
+  return <section>A <UserA /></section>
+}
+const B = () => {
+  return <section>B <UserModifier name='Input' /> </section>
+}
+const C = () => {
+  return <section>C <UserC /> </section>
+}
+const UserA = connect(({ state, dispatch }) => {
+  console.log('A Update', Math.random())
+  return <div>User: {state.user.name}</div>
+})
+const UserC = () => {
+  console.log('C Update', Math.random())
+  const { state } = useContext(appContext)
+  return <div>User: {state.user.name}</div>
 }
 
 const reducer = (state, { type, payload }) => {
@@ -39,15 +75,8 @@ const reducer = (state, { type, payload }) => {
   }
 }
 
-const connect = (Component) => (props) => {
-  const { state, setState } = useContext(appContext)
-  const dispatch = (action) => {
-    setState(reducer(state, action))
-  }
-  return <Component {...props} dispatch={dispatch} state={state} />
-}
-
 const UserModifier = connect(({ dispatch, state, ...rest }) => {
+  console.log('B Update', Math.random())
   const onChange = (e) => {
     // 1. 
     // state.user.name = e.target.value
@@ -59,7 +88,6 @@ const UserModifier = connect(({ dispatch, state, ...rest }) => {
   }
   return (
     <>
-      {console.log(rest)}
       <input value={state.user.name} onChange={onChange}></input>
     </>
   )
