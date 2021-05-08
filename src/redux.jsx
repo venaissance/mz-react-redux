fc9550dd57f832beebece9fd7668b753216d0c92
mize <mize@bytedance.com>
2021-05-09 01:04:37 +0800

    add comments for better understanding

--- src/redux.jsx
+++ src/redux.jsx
@@ -8,32 +8,18 @@ export const store = {
     },
     setState(newState) {
         store.state = newState
-        store.listeners.map(fn => fn(store.state))
+        store.listeners.map(fn => fn(store.state)) // state 修改时，触发每个 connect 组件的 update
     },
     listeners: [],
     subscribe(fn) {
         const l = store.listeners;
-        console.log(l)
         l.push(fn)
         return () => {
             const i = l.indexOf(fn)
+            console.log('cancel', fn)
             l.splice(i, 1)
         }
     }
-}
-
-export const connect = (Component) => (props) => {
-    const { state, setState } = useContext(appContext)
-    const [, update] = useState({})
-    useEffect(() => {
-        store.subscribe(() => {
-            update({})
-        })
-    }, [])
-    const dispatch = (action) => {
-        setState(reducer(state, action))
-    }
-    return <Component {...props} dispatch={dispatch} state={state} />
 }
 
 export const reducer = (state, { type, payload }) => {
@@ -46,3 +32,17 @@ export const reducer = (state, { type, payload }) => {
         return state
     }
 }
+
+export const connect = (Component) => (props) => {
+    const { state, setState } = useContext(appContext)
+    const [, update] = useState({})
+    useEffect(() => {
+        return store.subscribe(() => update({})) // 订阅每一个 connect 组件的 update
+    }, [])
+    const dispatch = (action) => {
+        setState(reducer(state, action))
+        update({}) // 需要通过修改 connect 组件的 state 来触发 render
+    }
+    return <Component {...props} dispatch={dispatch} state={state} />
+}
+
